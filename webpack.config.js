032ee0018c808dc96a7cd91fad2504ade2177907
Conor Wuertz <conor.wuertz@gmail.com>
2020-10-16 14:06:26 -0700

    setup gitignore, get local env up to speed, remove deprecated loaders array.

--- webpack.config.js
+++ webpack.config.js
@@ -8,15 +8,9 @@
          filename: 'main.bundle.js'
      },
      module: {
-         loaders: [
-             {
-                 test: /\.js$/,
-                 loader: 'babel-loader',
-                 query: {
-                     presets: ['es2015']
-                 }
-             }
-         ]
+        rules: [
+
+        ]
      },
      stats: {
          colors: true
