61a1531833531fa3d81ea9dcb21d1464d991be09
Conor Wuertz <conor.wuertz@gmail.com>
2020-10-16 17:15:03 -0700

    -add svg support. svgs smaller than 1MB will be rendered as a base64 encoded string

--- js/DummyComponent.jsx
+++ js/DummyComponent.jsx
@@ -3,19 +3,21 @@ import moonJPG from "../assets/moon.jpg"
 import moonJPEG from "../assets/moon.jpeg"
 import stickFigure from "../assets/stickfigure.png"
 import breakdancers from "../assets/breakdancers.gif"
+import germany from "../assets/germany.svg"
+import duck from "../assets/duck.svg"
 
 class DummyComponent extends React.Component {
 
     render() {
 
         return (<>
-                    <div className="dummyComponent">
-                    Hello world
-                </div>
+                <div className="dummyComponent"> Hello world </div>
                 <img src={moonJPG} alt="not found" />
                 <img src={moonJPEG} alt="not found" />
                 <img src={stickFigure} alt="not found" />
                 <img src={breakdancers} alt="not found" />
+                <img src={germany} alt="not found" />
+                <img src={duck} alt="not found" />
             </>)
     }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -5,41 +5,51 @@
  module.exports = {
      entry: ['./js/main.js','./scss/main.scss'],
      output: {
-         path: path.resolve(__dirname, 'build'),
-         filename: '[name].[fullhash].bundle.js'
+        path: path.resolve(__dirname, 'build'),
+        filename: '[name].[fullhash].bundle.js'
      },
-     module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                  loader: "babel-loader"
+    module: {
+    rules: [
+        {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: {
+                loader: "babel-loader"
+            }
+        },
+        {
+            test: /\.s[ac]ss$/i,
+            use: [
+                MiniCssExtractPlugin.loader,
+                'css-loader',
+                'sass-loader'
+            ]
+        },
+        {
+            test: /\.(png|jpg|jpeg|gif)$/,
+            use: [
+                'file-loader',
+            ],
+            },
+        {
+            test: /\.svg/,
+            use: {
+                loader: 'svg-url-loader',
+                options: {
+                    limit: 1048576,
+                    encoding: "base64"
                 }
-            },
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                  'css-loader',
-                  'sass-loader'
-                ]
-            },
-            {
-                test: /\.(png|jpg|jpeg|gif)$/,
-                use: [
-                  'file-loader',
-                ],
-              },
+            }
+            }
         ]
-     },
-     stats: {
-         colors: true
-     },
-     devtool: 'source-map',
-     plugins: [
-        new MiniCssExtractPlugin({
-            filename: 'style.[fullhash].css'
-          })
-      ],
+    },
+    stats: {
+        colors: true
+    },
+    devtool: 'source-map',
+    plugins: [
+    new MiniCssExtractPlugin({
+        filename: 'style.[fullhash].css'
+        })
+    ],
  };
